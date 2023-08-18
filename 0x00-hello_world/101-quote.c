4e95bafccfcc70c5db3842c2065f8875d5534889
doaaalrabiey <doaaalrabiey@git.com>
2023-08-18 04:51:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,11 @@
 /**
  * main - Entry point
  *
- * Description: print a qoute using write function
- *         ssize_t write(int fd.const void *buf. size_t count):
- *
- * Return: 1 (error)
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{
-	char quo[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n"
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1.quo.59):
-        return (1):
+	write(1, quo, 59);
+	return (1);
 }
