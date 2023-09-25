cffb49e235e50a6d4dccfa18317f5104bf2d541f
doaaalrabiey <doaaalrabiey@git.com>
2023-09-25 07:38:20 -0700

    ddddd

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -4,25 +4,59 @@
 #include "lists.h"
 
 /**
+ * find_listint_loop_fl - loop in a linked list
+ * @head: linked list to search
+ *
+ * Return: node or null
+ */
+
+listint_t *find_listint_loop_fl(listint_t *head)
+{
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
+
+
+
+
+
+
+
+/**
  * free_listint_safe - function that frees a listint_t list.
- * @head: pointer
+ * @h: pointer
  *
  * Return: the size of the list that was free’d
  */
 
 size_t free_listint_safe(listint_t **h)
 {
-        listint_t *next, *ioopnode;
-        size_t len;
+	listint_t *next, *loopnode;
 
-        if (h == NULL || *h == NULL)
-                return (0);
+	size_t len;
 
-        loopnode = find_listint_loop_fl(*h);
-        for (len = 0; (*h ! = loopnode || loop) && *h != NULL; *h = next)
-        {
-                len++;
-                next = (*h)->next;
+	int loop = 1;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	loopnode = find_listint_loop_fl(*h);
+	for (len = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
+	{
+		len++;
+		next = (*h)->next;
 		if (*h == loopnode && loop)
 		{
 			if (loopnode == loopnode->next)
@@ -36,6 +70,8 @@ size_t free_listint_safe(listint_t **h)
 			loop = 0;
 		}
 		free(*h);
-        }
+	}
+	*h = NULL;
+	return (len);
 
 }
