5e049e548ce1d75635b810540b7db8957c47b051
doaaalrabiey <doaaalrabiey@git.com>
2023-09-25 08:07:49 -0700

    sssss

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ * @separator: string
+ * @n: argument
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list ap;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (i--)
+		printf("%d%s", va_arg(ap, int),
+				i ? (separator ? separator : "") : "\n");
+	va_end(ap);
+}
