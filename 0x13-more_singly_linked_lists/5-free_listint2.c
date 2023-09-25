e727f08b026839464d36133339acf87d1249aa2a
doaaalrabiey <doaaalrabiey@git.com>
2023-09-25 01:43:22 -0700

    tttttt

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: pointer
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *temp;
+
+	if (!head)
+		return;
+
+	node = *head;
+	while (node)
+	{
+		temp = node;
+		node = node->next;
+		free(temp);
+	}
+	*head = NULL;
+}
