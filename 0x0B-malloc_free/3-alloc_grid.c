c8aeb68c32747972a2663666e96ce0df7049eb4d
doaaalrabiey <doaaalrabiey@git.com>
2023-10-30 09:54:44 -0700

    ddd

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * **alloc_grid - entry point
+ * @width: int
+ * @height: int
+ * Return: array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **fun, h, y;
+
+	fun = malloc(sizeof(*fun) * height);
+
+	if (width <= 0 || height <= 0 || fun == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (h = 0; h < height; h++)
+		{
+			fun[h] = malloc(sizeof(**fun) * width);
+			if (fun[h] == 0)
+			{
+				while (h--)
+
+				free(fun);
+				return (NULL);
+			}
+
+			for (y = 0; y < width; y++)
+				fun[h][y] = 0;
+		}
+	}
+
+	return (fun);
+}
