9846521c5e600c6d6bc493b7b92c7ca3cd80d360
doaaalrabiey <doaaalrabiey@git.com>
2023-11-07 14:50:19 -0800

    dddd

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - entry point
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
