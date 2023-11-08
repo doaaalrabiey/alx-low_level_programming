62dadf95ff4bb1ac9946c76a1d454a40f112cead
doaaalrabiey <doaaalrabiey@git.com>
2023-11-08 14:24:30 -0800

    ss

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - entry point
+ * @array: int
+ * @size: array size
+ * @cmp: compare function
+ * Return: integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+
+			i++;
+		}
+	return (-1);
+}
