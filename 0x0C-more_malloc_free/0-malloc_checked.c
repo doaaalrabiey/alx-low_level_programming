82a3efbf2e2021e83139dc404644d92b768ca91f
doaaalrabiey <doaaalrabiey@git.com>
2023-11-07 16:29:18 -0800

    j

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - entry point
+ * @b: int
+ * Return: pointer
+ *
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+
+	return (m);
+}
