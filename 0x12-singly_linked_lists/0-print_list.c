25e68b43d2eeef5e9910791b70b2c5235ed209a4
doaaalrabiey <doaaalrabiey@git.com>
2023-11-16 14:58:24 -0800

    d

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+
+/**
+ * _strlen - entry point
+ * @s: string
+ *
+ * Return: integer
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - entry point
+ * @h: pointer
+ *
+ * Return: size
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
