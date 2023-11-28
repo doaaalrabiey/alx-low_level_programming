84c9f7d1dc6296d8dd1f5bfa10762abbaacfff33
doaaalrabiey <doaaalrabiey@git.com>
2023-11-28 00:25:06 -0800

    dd

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,18 +9,18 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int dhy;
 	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
-	fd + open(filename, O_RDONLY);
-	if (fd == -1)
+	dhy = open(filename, O_RDONLY);
+	if (dhy == -1)
 		return (0);
-	bytes = read(fd, &buf[0], letters);
+	bytes = read(dhy, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	close(fd);
+	close(dhy);
 	return (bytes);
 
 }
